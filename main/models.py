3d093a1d425c3c3c2066a107a37dfa77b1a873b9
Abduqodir <ttheonepersonn@gmail.com>
2025-11-06 12:15:00 +0500

    files model

--- main/models.py
+++ main/models.py
@@ -1,8 +1,9 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 
 
 class Files(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
     title=models.CharField(default='no title', max_length=50)
     file=models.FileField(upload_to='files/')
     uploaded_at=models.DateTimeField(auto_now_add=True)
